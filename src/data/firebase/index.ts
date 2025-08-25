52d89b5da39c5f5d7c11879f2d54d8fb23c72412
Alex Cuenca <alex.cuenca@expotuna.com>
2025-08-25 16:21:12 -0500

    adding credentials

--- src/data/firebase/index.ts
+++ src/data/firebase/index.ts
@@ -1,1 +1,9 @@
-console.log("first")
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
+import serviceAccount from '../../../serviceAccountKey.json';
+
+initializeApp({
+  credential: cert(serviceAccount as unknown as string)
+});
+
+export const db = getFirestore();
