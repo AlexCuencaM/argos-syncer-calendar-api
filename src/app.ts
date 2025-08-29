b0b5a9896d9ce20e77a116a3d38e4a08b4da16dd
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2025-08-28 22:42:02 -0500

    Adding console server

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import { envs } from "./config/envs";
 import { AppRoutes } from "./presentation/API/routes";
 import { ApiServer } from "./presentation/API/api-server";
+import { ConsolerServer } from "./presentation/Console/console-server";
 
 (async ()=>{
     await main();
 })();
 
 async function main () {
-    const server = new ApiServer({
+    const apiServer = new ApiServer({
         port: envs.PORT,
         routes: AppRoutes.routes,
     });
-    await server.start();
+    await apiServer.start();
+    const consoleServer = new ConsolerServer({
+        iscUrl: envs.CALENDAR_ICS_URL,
+    });
+    await consoleServer.start();
 }
